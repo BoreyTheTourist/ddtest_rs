6203988c3d2f49b0fc780284911766fcab5d424d
Boris Milchenko <borey_the_tourist@mail.ru>
2023-10-20 23:02:39 +0300

    Fixed: imposssibility to use on different functions

--- src/lib.rs
+++ src/lib.rs
@@ -129,7 +129,6 @@ fn impl_test_data(attr_ast: &FieldValue, item_ast: &ItemFn) -> TokenStream {
     };
     let proxy_name = &item_ast.sig.ident;
     let arguments_count = item_ast.sig.inputs.len();
-    let test_name = "test";
     let mut gen = String::new();
     let mut gen_cur;
     let mut name_cur;
@@ -145,7 +144,7 @@ fn impl_test_data(attr_ast: &FieldValue, item_ast: &ItemFn) -> TokenStream {
             arguments = format!("{}, {}[{}].{}", arguments, data_name, test_count, i);
             i += 1;
         }
-        name_cur = format_ident!("{}_{}", test_name, test_count);
+        name_cur = format_ident!("{}_{}", proxy_name, test_count);
         call = format!("{}({})", proxy_name, arguments);
         gen_cur = format!("#[test]\nfn {}() {{{}}}", name_cur, call);
         gen = format!("{}{}", gen, gen_cur);
